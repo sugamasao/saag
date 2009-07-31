416e6a636724219f23f1a07618bc05ac7edfda78
sugamasao <sugamasao@gmail.com>
2009-08-01 05:47:17 +0900

    add spec(empty files)

---
+++ spec/spec_helper.rb
@@ -0,0 +1,11 @@
+require 'spec'
+
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'saag'
+
+Spec::Runner.configure do |config|
+  
+end
+
+
